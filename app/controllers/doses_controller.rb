4f1112ea72a0f499cca8f3bb2c22fcfe71276f7a
Jess Lee <jessclee92@me.com>
2018-08-17 10:52:59 +0100

    mister cocktail backend

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -7,16 +7,19 @@ class DosesController < ApplicationController
   def create
     @cocktail = Cocktail.find(params[:cocktail_id])
     @dose = Dose.new(dose_params)
-    @dose.cocktail = Cocktail.find(params[:cocktail_id])
-    if dose.save
+    # @ingredient = Ingredient.find(params[:dose][:ingredient])
+    @dose.cocktail = @cocktail
+    # raise
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
     else
-    render :new
+      render :new
     end
   end
 
   private
 
   def dose_params
-    params.require(:doses).permit(:description)
+    params.require(:dose).permit(:description, :ingredient_id)
   end
 end
